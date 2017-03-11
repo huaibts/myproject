948c2807ffbe671e12b8df7003814b3ddadf0a27
Huaijung Huang <je3rjoy@gmail.com>
2017-03-11 19:00:23 +0800

    The progress of abstraction(homework)

---
+++ src/oo/abstraction/Motorcycle.java
@@ -0,0 +1,17 @@
+package oo.abstraction;
+
+public class Motorcycle {
+	String brand;
+	String name;
+	String color;
+	int weight;
+	int cc;
+	
+	public Motorcycle(String brand,String name,String color,int weight,int cc){
+		this.brand = brand;
+		this.name = name;
+		this.color = color;
+		this.weight = weight;
+		this.cc = cc;
+	}
+}
---
+++ src/oo/abstraction/MotorcycleTester.java
@@ -0,0 +1,10 @@
+package oo.abstraction;
+
+public class MotorcycleTester {
+
+	public static void main(String[] args) {
+		Motorcycle m1 = new Motorcycle("Yamaha","Ray","white",106,125);
+
+	}
+
+}
