28fcbf11a514f6eb5613f251ec300e2be1cc690c
ASUS-WIN10 <ASUS-WIN10@DESKTOP-TF07IU4>
2017-06-13 13:02:03 +0800

    arraylist add

--- src/oo/shopping2/Tester.java
+++ src/oo/shopping2/Tester.java
@@ -3,12 +3,18 @@ package oo.shopping2;
 import java.util.ArrayList;
 
 import oo.shoping.Customer;
+import oo.shoping.GoldenCustomer;
+import oo.shoping.SilverCustomer;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Customer> list = new ArrayList<>();
+		list.add(new GoldenCustomer(500));
+		list.add(new SilverCustomer(65040));
+		list.add(new Customer(63210));
+		list.add(new GoldenCustomer(8054));
 	}
 
 }
